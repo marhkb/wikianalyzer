b94897406a7b56c6f28b9cc5f49477f91c283d58
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2013-04-23 17:06:52 +0200

    added jdoc and licence

--- wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/server/MainServiceImpl.java
+++ wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/server/MainServiceImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Marcus Behrendt & Robert Friedrichs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+ 
 package de.behrfried.wikianalyzer.wawebapp.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -5,8 +21,21 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import de.behrfried.wikianalyzer.wawebapp.client.service.MainService;
 import de.behrfried.wikianalyzer.wawebapp.shared.StringObject;
 
+/**
+ * Server side implementation of {@link MainService}
+ * @author marcus
+ *
+ */
 public class MainServiceImpl extends RemoteServiceServlet implements MainService {
 
+    /**
+     * generated UID
+     */
+    private static final long serialVersionUID = 200675849907147502L;
+
+    /**
+     * Look at {@link MainService}
+     */
     public StringObject getStringObject(StringObject o) {
 	return o;
     }
--- wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/shared/StringObject.java
+++ wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/shared/StringObject.java
@@ -1,27 +1,66 @@
+/*
+ * Copyright 2013 Marcus Behrendt & Robert Friedrichs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+ 
 package de.behrfried.wikianalyzer.wawebapp.shared;
 
 import java.io.Serializable;
 
+/**
+ * Wrapper around an {@link String} that is used for client server communication
+ * @author marcus
+ *
+ */
 public class StringObject implements Serializable {
     
     /**
-     * 
+     * generated UID
      */
     private static final long serialVersionUID = -6407435912570206085L;
     
+    /**
+     * the String instance
+     */
     private String str;
     
+    /**
+     * Default constructor for {@link Serializable}
+     */
     public StringObject() {  }
 
+    /**
+     * Creates a {@link StringObject} instance
+     * @param str the {@link String} to be held by this {@link StringObject}
+     */
     public StringObject(String str) {
 	super();
 	this.str = str;
     }
 
+    /**
+     * Returns the inner {@link String}.
+     * @return the inner {@link String}
+     */
     public String getStr() {
         return this.str;
     }
 
+    /**
+     * Sets the inner {@link String}.
+     * @param str the inner {@link String} to be set
+     */
     public void setStr(String str) {
         this.str = str;
     }
