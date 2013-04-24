37e661c3f2f733d42f0bda705126a7579a4e80e2
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2013-04-24 12:53:02 +0200

    prework is done

--- wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/client/presenter/ArticlePresenter.java
+++ wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/client/presenter/ArticlePresenter.java
@@ -1,5 +1,12 @@
 package de.behrfried.wikianalyzer.wawebapp.client.presenter;
 
+import de.behrfried.wikianalyzer.wawebapp.client.view.ArticleView;
+
+/**
+ * Interface for the {@link ArticleView}'s Presenter.
+ * @author marcus
+ *
+ */
 public interface ArticlePresenter extends Presenter {
     
 }
--- wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/client/view/DefaultArticleView.java
+++ wawebapp/src/main/java/de/behrfried/wikianalyzer/wawebapp/client/view/DefaultArticleView.java
@@ -1,12 +1,34 @@
 package de.behrfried.wikianalyzer.wawebapp.client.view;
 
-public class DefaultArticleView implements ArticleView {
+import com.google.inject.Inject;
 
+/**
+ * Default implementation of {@link ArticleView}.
+ * @author marcus
+ *
+ */
+public class DefaultArticleView implements ArticleView {
+    
+    /**
+     * the parent element of the {@link DefaultArticleView} where the widgets has to be put in.
+     */
+    private final StartPageView parentView;
+    
+    @Inject
+    public DefaultArticleView(StartPageView parentView) {
+	this.parentView = parentView;
+    }
+    
+    /**
+     * @see View
+     */
     public void init() {
 	// TODO Auto-generated method stub
-	
     }
-
+    
+    /**
+     * @see View
+     */
     public void dispose() {
 	// TODO Auto-generated method stub
 	
